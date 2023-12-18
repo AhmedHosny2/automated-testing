90490e3d3d12d4939b1e65edcffcf5b3c1702675
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-12-18 21:56:23 +0200

    add fire fox option

--- setUpDrive.py
+++ setUpDrive.py
@@ -1,5 +1,5 @@
-# set up the programmer
-
+# # set up the programmer using chrome
+#
 from selenium import webdriver
 
 from selenium.webdriver.chrome.options import Options
@@ -12,3 +12,18 @@ def setup_driver():
     chrome_options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=chrome_options)
     return driver
+
+#using fire fox
+
+# from selenium import webdriver
+# from selenium.webdriver.firefox.options import Options
+#
+#
+# def setup_driver():
+#     # Set up Firefox options
+#     firefox_options = Options()
+#     # Add any additional options if needed
+#
+#     # Create a Firefox WebDriver instance
+#     driver = webdriver.Firefox(options=firefox_options)
+#     return driver
