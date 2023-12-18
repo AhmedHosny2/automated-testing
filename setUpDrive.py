186dd5e9fc4b31ab974e6e861fd335fa470b4a91
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-12-18 21:46:51 +0200

    create shopping cart test

--- setUpDrive.py
+++ setUpDrive.py
@@ -3,7 +3,6 @@
 from selenium import webdriver
 
 from selenium.webdriver.chrome.options import Options
-import time
 
 
 
--- shoppinCart.py
+++ shoppinCart.py
@@ -1,10 +1,44 @@
 # main_script.py
 
 from signin import signIn
-from setUpDrive import setup_driver
+import time
 
 # Replace these with actual values
 username = "a@a.com"
 password = "P,V2t@+%d^UnQBt"
 
 driver = signIn(username, password)
+
+
+def add_to_cart(productName, quantity):
+    quantity -= 1
+    clothes_button = driver.find_element("xpath", '//*[@id="category-3"]/a')
+    clothes_button.click()
+    # woman_section_button = driver.find_element("xpath",
+    #                                            "//a[contains(@class, 'dropdown-item') and contains(@href, 'women')]")
+    # woman_section_button.click()
+    sweater_section_button = driver.find_element("xpath", f"//a[contains(@href, '{productName}')]")
+    sweater_section_button.click();
+    add_to_cart_button = driver.find_element("class name", "btn-primary")
+    # quantity_wanted_field = driver.find_element("id", "quantity_wanted")
+    # quantity_wanted_field.clear()
+    # quantity_wanted_field.send_keys(quantity)
+
+    quantity_wanted_field = driver.find_element("class name", "btn-touchspin")
+
+    for _ in range(quantity):
+        quantity_wanted_field.click()
+
+    add_to_cart_button.click()
+    check_out_button = driver.find_element("xpath",
+                                           "//a[contains(@class, 'btn') and contains(@class, 'btn-primary') and contains(@href, 'cart?action=show')]")
+    check_out_button.click()
+    time.sleep(2)
+
+
+if __name__ == "__main__":
+    # Replace these with actual values
+    url = "https://nondescript-loaf.demo.prestashop.com/en/women/2-9-brown-bear-printed-sweater.html#/1-size-s"
+    quantity = 3
+    productName = "sweater"
+    add_to_cart(productName, quantity)
